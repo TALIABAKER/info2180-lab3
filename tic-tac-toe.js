ff14b6590346dc4958b7f514a6c877ba043fed07
Talia Baker <taliabaker@Talias-MacBook-Air.local>
2023-10-23 22:30:46 -0500

    UpdatedExercises

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,5 @@
-window.onload= function()
-{
+window.onload = function () {
+    
     const status = document.getElementById("status");
     const board = document.getElementById("board");
     const button = document.getElementsByClassName("btn")[0];
@@ -8,8 +8,7 @@ window.onload= function()
     let player = 'X';
     let sqr_pos = ['', '', '', '', '', '', '', '', ''];
 
-    const winningConditions = 
-    [
+    const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -20,70 +19,56 @@ window.onload= function()
         [2, 4, 6]
     ];
 
-
-    for(let j=0; j<=8; j++)
-    {
-        gmesqr[j].setAttribute("class","square");
+    for (let j = 0; j <= 8; j++) {
+        gmesqr[j].setAttribute("class", "square");
     }
 
-    gmesqr.forEach((seg, position) => 
-    {
+    gmesqr.forEach((seg, position) => {
         seg.addEventListener('click', () => userAction(seg, position));
-        seg.addEventListener('mouseover', function()
-        {
-        seg.classList.add('hover');
-       // seg.removeEventListener('click', () => userAction(seg, position))
+        seg.addEventListener('mouseover', function () {
+            seg.classList.add('hover');
         });
-        seg.addEventListener('mouseout', function(){
+        seg.addEventListener('mouseout', function () {
             seg.classList.remove('hover');
         });
     });
 
-    function checkWin()
-    {
-        for(let i=0; i<=7; i++){
+    function checkWin() {
+        for (let i = 0; i <= 7; i++) {
             const win = winningConditions[i];
- 
-            const pos1= sqr_pos[win[0]];
+
+            const pos1 = sqr_pos[win[0]];
             const pos2 = sqr_pos[win[1]];
             const pos3 = sqr_pos[win[2]];
-            if (pos1 === ''|| pos2 === ''|| pos3===''){
+            if (pos1 === '' || pos2 === '' || pos3 === '') {
                 continue;
-            }  
-            if (pos1 === pos2 && pos2 === pos3)
-            {
-                status.innerHTML= 'Congratulations!' +  pos1 +' is the winner';
+            }
+            if (pos1 === pos2 && pos2 === pos3) {
+                status.innerHTML = 'Congratulations! ' + pos1 + ' is the winner';
                 status.classList.add('you-won');
                 break;
-            }     
+            }
         }
     }
-    const userAction= (seg,position) =>
-    {
-        console.log(seg.innerText)
 
-        if(seg.innerText !== 'X' || seg.innerText !== 'O')
-        {
-            seg.innerText= player;
+    const userAction = (seg, position) => {
+        if (seg.innerText !== 'X' && seg.innerText !== 'O') {
+            seg.innerText = player;
             seg.classList.add(player);
-            sqr_pos[position]=player;
-            console.log(sqr_pos);
+            sqr_pos[position] = player;
             checkWin();
-            if (player === 'X' ? player ='O' : player ='X');
+            player = (player === 'X') ? 'O' : 'X';
         }
-
     }
 
-    button.addEventListener('click', ()=>
-    {
-        sqr_pos=['', '', '', '', '', '', '', '', ''];
-        status.innerHTML= 'Move your mouse over a square and click to play an X or an O.'
+    button.addEventListener('click', () => {
+        sqr_pos = ['', '', '', '', '', '', '', '', ''];
+        status.innerHTML = 'Move your mouse over a square and click to play an X or an O.';
         status.classList.remove('you-won');
-        gmesqr.forEach(seg =>
-            {
-            seg.innerText ='';
+        gmesqr.forEach(seg => {
+            seg.innerText = '';
             seg.classList.remove('X');
             seg.classList.remove('O');
         });
     });
-}
+};
